e2a618b28ccf9aaa0bd2b694f0fa7424c3a60d55
KI Serge Laobien <sergelaobienk@gmail.com>
2020-03-19 21:11:50 +0000

    ClasseMetierTest mis a jour

--- eclipse-workspace/Pglp3-5/src/test/java/PGLP_3/Pglp3_5/ClasseMetierTest.java
+++ eclipse-workspace/Pglp3-5/src/test/java/PGLP_3/Pglp3_5/ClasseMetierTest.java
@@ -1,5 +1,19 @@
 package PGLP_3.Pglp3_5;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class ClasseMetierTest {
+ClasseMetier metier;
+	
+	@Before
+	public void setMetier() {
+		this.metier = new ClasseMetier();;
+	}
+
+	@Test
+	public void test() {
+		metier.UneMethodeMetier();
+	}
 
 }
